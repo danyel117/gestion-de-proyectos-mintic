7eabbc5daa1037e841d2471d363d0a10fcdd287d
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2021-11-02 18:42:54 +0000

    agregar link para ir al home desde la pagina de /admin/usuarios

--- pages/admin/usuarios/index.tsx
+++ pages/admin/usuarios/index.tsx
@@ -1,7 +1,15 @@
+import Link from 'next/link';
 import React from 'react';
 
 const index = () => {
-  return <div>Pagina de admin de usuarios</div>;
+  return (
+    <div>
+      <div>Pagina de admin de usuarios</div>
+      <Link href='/'>
+        <a>Ir al Home</a>
+      </Link>
+    </div>
+  );
 };
 
 export default index;
