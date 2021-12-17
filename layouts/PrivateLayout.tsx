8273fbf4cdbf953766c502ff884fe7242783a6f8
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2021-12-17 18:02:40 +0000

    agregar migracion

--- layouts/PrivateLayout.tsx
+++ layouts/PrivateLayout.tsx
@@ -2,13 +2,20 @@ import React, { useEffect } from 'react';
 import { useSession, signIn, signOut } from 'next-auth/react';
 
 const PrivateLayout = ({ children }) => {
-  const { data: session, status } = useSession();
+  const { data: session, status }: any = useSession();
+
+  useEffect(() => {
+    console.log(session);
+  }, [session]);
 
   if (status === 'loading') return <div>Loading...</div>;
 
   if (!session) {
     signIn('auth0');
   } else {
+    if (session.user.estado === 'Pendiente') {
+      return <div>Usted no ha sido autorizado para ingresar aun</div>;
+    }
     return (
       <div className='flex flex-col'>
         <button
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,11 +2,25 @@ import NextAuth from 'next-auth';
 import Auth0Provider from 'next-auth/providers/auth0';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { PrismaClient } from '@prisma/client';
+import { Usuario } from 'prisma/generated/type-graphql';
 
 const prisma = new PrismaClient();
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      if (user.estado === 'NoAutorizado') return false;
+
+      return true;
+    },
+    async session({ session, user, token }) {
+      console.log(session, user, token);
+      const session2 = { ...session, user };
+      return session2;
+    },
+  },
+
   providers: [
     Auth0Provider({
       clientId: process.env.AUTH0_CLIENT_ID,
